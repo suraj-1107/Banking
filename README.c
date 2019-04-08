3ed3aefdd1e4bcf6d062d0b0f908c44fa0f418de
suraj-1107 <49410726+suraj-1107@users.noreply.github.com>
2019-04-08 23:45:27 +0530

    allocation

--- README.c
+++ README.c
@@ -43,3 +43,29 @@ int main(int argc, char** argv) {
         maxRequired = (int **)malloc(nProcesses * sizeof(*maxRequired));
         for(int i=0; i<nProcesses; i++)
                 maxRequired[i] = (int *)malloc(nResources * sizeof(**maxRequired));
+		 // allocated
+        printf("\n");
+        for(int i=0; i<nProcesses; i++) {
+                printf("\nResource allocated to process %d (R1 R2 ...)? ", i+1);
+                for(int j=0; j<nResources; j++)
+                        scanf("%d", &allocated[i][j]);
+        }
+        printf("\n");
+
+	// maximum required resources
+        for(int i=0; i<nProcesses; i++) {
+                printf("\nMaximum resource required by process %d (R1 R2 ...)? ", i+1);
+                for(int j=0; j<nResources; j++)
+                        scanf("%d", &maxRequired[i][j]);
+        }
+        printf("\n");
+
+	// calculate need matrix
+        need = (int **)malloc(nProcesses * sizeof(*need));
+        for(int i=0; i<nProcesses; i++)
+                need[i] = (int *)malloc(nResources * sizeof(**need));
+
+        for(int i=0; i<nProcesses; i++)
+                for(int j=0; j<nResources; j++)
+                        need[i][j] = maxRequired[i][j] - allocated[i][j];
+
